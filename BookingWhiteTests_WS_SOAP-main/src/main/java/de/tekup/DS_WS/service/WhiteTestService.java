8617af6083c536f32987674d8df9b937a7536390
karboulx <fkarboul@gmail.com>
2021-05-27 22:31:40 +0100

    first commit

---
+++ BookingWhiteTests_WS_SOAP-main/src/main/java/de/tekup/DS_WS/service/WhiteTestService.java
@@ -0,0 +1,94 @@
+package de.tekup.DS_WS.service;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.springframework.stereotype.Service;
+
+import de.tekup.DS_WS.DsWsSoapApplication;
+import de.tekup.soap.models.whitetest.Exam;
+import de.tekup.soap.models.whitetest.ObjectFactory;
+import de.tekup.soap.models.whitetest.StudentRequest;
+import de.tekup.soap.models.whitetest.WhiteTestResponse;
+import de.tekup.soap.models.whitetest.Student;
+import de.tekup.soap.models.whitetest.Student.Address;
+
+@Service
+public class WhiteTestService {
+	
+	public  List<Student> students = new ArrayList<Student>();
+	public  List<Exam> exams = new ArrayList<Exam>();
+
+
+	public WhiteTestResponse checkExam(StudentRequest studentRequest) throws DatatypeConfigurationException
+	{
+		
+		Address a1 = new Address();
+		a1.setCity("Ariena");
+		a1.setStreet("rue hbib bourgiba");
+		a1.setPosteCode("8050");
+		
+		Address a2 = new Address();
+		a2.setCity("Beja");
+		a2.setStreet("Rue becha");
+		a2.setPosteCode("9070");
+		
+		Address a3 = new Address();
+		a3.setCity("Sousse");
+		a3.setStreet("Rue mourouj");
+		a3.setPosteCode("5020");
+		
+		Address falseAddress = new Address();
+		falseAddress.setCity("ERREUR");
+		falseAddress.setStreet("ERREUR");
+		falseAddress.setPosteCode("ERREUR");
+		
+		students.add(new Student(1,"student1",a1));
+		students.add(new Student(2,"student2",a2));
+		students.add(new Student(3,"student3",a3));
+		
+		exams.add(new Exam("code_OCA","OCA"));
+		exams.add(new Exam("code_OCP","OCP"));
+		
+		Student falseStudent = new Student (-1, "erreur",falseAddress);
+		Exam falseExam = new Exam ("-1","erreur");
+		
+		WhiteTestResponse response = new ObjectFactory().createWhiteTestResponse();
+		
+				response.setExam(falseExam);
+		response.setStudent(falseStudent);
+		
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
+        XMLGregorianCalendar now = 
+            datatypeFactory.newXMLGregorianCalendar(gregorianCalendar);
+		
+		for (Student student : students) { 		
+
+	           if (student.getId() == studentRequest.getStudentId())
+	           {
+	        	   System.out.println("student id : "+student.getId());
+	        	   System.out.println("studentRequest id : "+studentRequest.getStudentId());
+	        	   System.out.println(student.getId() == studentRequest.getStudentId());
+	        	   System.out.println(student.toString());
+	        	   response.setStudent(student);
+	           }
+	      }
+		for (Exam exam : exams)
+		{
+			if (exam.getCode().equals(studentRequest.getExamCode()))
+			{
+				response.setExam(exam);
+				response.setDate(now);
+			}
+		}
+
+		return response;
+	}
+
+}
---
+++ WhiteTest_WS_Consumer-main/src/main/java/de/tekup/DS/soap/api/consume/whitetests/package-info.java
@@ -0,0 +1,9 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.0 
+// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2021.05.26 à 10:42:06 PM GMT+01:00 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tekup.de/soap/models/whitetest", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package de.tekup.DS.soap.api.consume.whitetests;
